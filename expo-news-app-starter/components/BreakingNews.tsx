3f38879ac27085c982ec9ca5c7cbf6d32ff3f092
duranginethmini <durangineth02@gmail.com>
2025-02-27 17:31:13 +0530

    added header and searchbar components

---
+++ expo-news-app-starter/components/BreakingNews.tsx
@@ -0,0 +1,15 @@
+import {StyleSheet,Text, View} from "react-native";
+
+type Props = {}
+
+const BreakingNews = (props: Props) => {
+    return (
+        <View>
+            <Text>BreakingNews</Text>
+        </View>
+    )
+}
+
+export default BreakingNews;
+
+const styles = StyleSheet.create({})
