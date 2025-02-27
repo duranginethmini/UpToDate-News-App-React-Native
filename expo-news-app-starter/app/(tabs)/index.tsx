c3c0bd91964134936e2b45d3f1316a8ecbb62b3d
duranginethmini <durangineth02@gmail.com>
2025-02-27 17:28:21 +0530

    home page and add,get news

--- expo-news-app-starter/app/(tabs)/index.tsx
+++ expo-news-app-starter/app/(tabs)/index.tsx
@@ -1,12 +1,39 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {useSafeAreaInsets} from "react-native-safe-area-context";
+import Header from "@/components/Header";
+import SearchBar from "@/components/SearchBar";
+import axios from "axios";
+import {NewsDataType} from "@/types";
+import BreakingNews from "@/components/BreakingNews";
 
 type Props = {}
 
 const Page = (props: Props) => {
+  const {top:safeTop} = useSafeAreaInsets();
+  const [breakingNews,setBreakingNews] = useState <NewsDataType[]>([])
+    useEffect(() => {
+        getBreakingNews()
+    },[]);
+
+  const getBreakingNews = async() => {
+         try {
+           const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&country=lk&language=en&image=1&size=5`
+           //   const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY }&country=in,lk&language=en,si&category=education,politics,science,technology,world&image=1&removeduplicate=1&size=5`
+             const response = await axios .get(URL);
+
+           if (response && response.data) {
+               setBreakingNews(response.data.results)
+           }
+         } catch (err:any) {
+             console.error(err)
+         }
+  }
   return (
-    <View style={styles.container}>
-      <Text>Home Screen</Text>
+    <View style={[styles.container,{paddingTop:safeTop}]}>
+     <Header/>
+      <SearchBar/>
+        <BreakingNews/>
     </View>
   )
 }
@@ -16,7 +43,6 @@ export default Page
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+
   },
 })
