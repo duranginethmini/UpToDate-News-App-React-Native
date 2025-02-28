4e02ee7d959daadc2912e5a176a2d17560331ce6
duranginethmini <durangineth02@gmail.com>
2025-02-28 12:53:04 +0530

    filter categories

--- expo-news-app-starter/app/(tabs)/index.tsx
+++ expo-news-app-starter/app/(tabs)/index.tsx
@@ -9,6 +9,7 @@ import BreakingNews from "@/components/BreakingNews";
 import Categories from "@/components/Categories";
 import categories from "@/components/Categories";
 import NewsList from "@/components/NewsList";
+import Loading from "@/components/Loading";
 
 
 type Props = {}
@@ -16,17 +17,20 @@ type Props = {}
 const Page = (props: Props) => {
   const {top:safeTop} = useSafeAreaInsets();
   const [breakingNews,setBreakingNews] = useState <NewsDataType[]>([])
-  const [isLoading, setIsLoading] = useState(true);
+    const [News,setNews] = useState <NewsDataType[]>([])
+
+    const [isLoading, setIsLoading] = useState(true);
     useEffect(() => {
-        getBreakingNews()
+        getBreakingNews();
+        getNews();
     },[]);
 
   const getBreakingNews = async() => {
          try {
-           const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&country=lk&language=en&image=1&size=5`
+           const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&language=en&image=1&size=5`
            //   const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY }&country=in,lk&language=en,si&category=education,politics,science,technology,world&image=1&removeduplicate=1&size=5`
              const response = await axios .get(URL);
-           console.log(response.data);
+          // console.log(response.data);
            if (response && response.data) {
                setBreakingNews(response.data.results)
                setIsLoading(false);
@@ -35,20 +39,40 @@ const Page = (props: Props) => {
              console.error(err)
          }
   }
+    const getNews = async(category:string = '') => {
+        try {
+            let categoryString = '';
+            if(category.length !== 0 ) {
+                categoryString = `&category=${category}`;
+            }
+            const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY}&language=en&image=1&size=10${categoryString}`;
+            //   const URL = `https://newsdata.io/api/1/news?apikey=${process.env.EXPO_PUBLIC_API_KEY }&country=in,lk&language=en,si&category=education,politics,science,technology,world&image=1&removeduplicate=1&size=5`
+            const response = await axios .get(URL);
+          //  console.log(response.data);
+            if (response && response.data) {
+                setNews(response.data.results)
+                setIsLoading(false);
+            }
+        } catch (err:any) {
+            console.error(err)
+        }
+    }
   const onCatChanged = (category:string) => {
       console.log('Categories',category);
+      setNews([]);
+      getNews(category);
   }
   return (
     <ScrollView style={[styles.container,{paddingTop:safeTop}]}>
      <Header/>
       <SearchBar/>
         {isLoading ? (
-            <ActivityIndicator size={'large'} />
+            <Loading size={'large'} />
         ) : (
             <BreakingNews newsList={breakingNews}/>
         )}
         <Categories onCategoryChanged={onCatChanged}/>
-        <NewsList newsList={breakingNews}/>
+        <NewsList newsList={News}/>
     </ScrollView>
   )
 }
--- expo-news-app-starter/components/NewsList.tsx
+++ expo-news-app-starter/components/NewsList.tsx
@@ -1,27 +1,96 @@
-import {Props} from "expo-system-ui/plugin/build/withAndroidUserInterfaceStyle";
-import {StyleSheet,View,Text} from "react-native";
-import {NewsDataType} from "@/types";
-import items from "ajv/lib/vocabularies/applicator/items";
+import { StyleSheet, View, Text, Image } from "react-native";
+import { NewsDataType } from "@/types";
+import { Colors } from "@/constants/Colors";
+import Loading from "@/components/Loading";
 
-type props = {
-    newsList :Array<NewsDataType>
-}
+type Props = {
+    newsList: Array<NewsDataType>;
+};
 
-const NewsList = ({newsList}: Props) => {
+const NewsList = ({ newsList }: Props) => {
+    if (newsList.length === 0) {
+        return (
+            <View style={styles.loadingContainer}>
+                <Loading size="large" />
+            </View>
+        );
+    }
+
     return (
         <View style={styles.container}>
-            {newsList.map((item,index) => (
-                <View key={index}>
-                    <Text>{item.title}</Text>
-                </View>
-            ))}
+            {newsList.map((item, index) => {
+                return (
+                    <View key={item.article_id || index} style={styles.itemContainer}>
+                        <Image source={{ uri: item.image_url }} style={styles.itemImg} />
+                        <View style={styles.itemInfo}>
+                            <Text style={styles.itemCategory}>{item.category}</Text>
+                            <Text style={styles.itemTitle}>{item.title}</Text>
+                            <View style={styles.itemSourceInfo}>
+                                <Image source={{ uri: item.source_icon }} style={styles.itemSourceImg} />
+                                <Text style={styles.itemSourceName}>{item.source_name}</Text>
+                            </View>
+                        </View>
+                    </View>
+                );
+            })}
         </View>
-    )
-}
-export default NewsList
+    );
+};
+
+export default NewsList;
 
 const styles = StyleSheet.create({
-     container: {
-         marginHorizontal: 20,
-     }
-    })
+    container: {
+        marginHorizontal: 20,
+        marginBottom: 50,
+    },
+    loadingContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginVertical: 20,
+    },
+    itemContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginBottom: 20,
+        flex: 1,
+        gap: 10,
+    },
+    itemImg: {
+        width: 90,
+        height: 100,
+        borderRadius: 20,
+        marginRight: 10,
+    },
+    itemInfo: {
+        flex: 1,
+        gap: 10,
+        justifyContent: "space-between",
+    },
+    itemCategory: {
+        fontSize: 12,
+        color: Colors.darkGrey,
+        textTransform: "capitalize",
+    },
+    itemTitle: {
+        fontSize: 12,
+        fontWeight: "600",
+        color: Colors.black,
+    },
+    itemSourceInfo: {
+        flexDirection: "row",
+        gap: 8,
+        alignItems: "center",
+    },
+    itemSourceImg: {
+        width: 20,
+        height: 20,
+        borderRadius: 20,
+    },
+    itemSourceName: {
+        fontSize: 10,
+        fontWeight: "400",
+        color: Colors.darkGrey,
+    },
+});
